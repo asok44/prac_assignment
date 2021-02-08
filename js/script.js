0e781a29a9e7f8f028c22ac2b3df49362b786906
Ashok Kumar Sunuwar <sunuwarashok99@gmail.com>
2021-02-08 20:02:44 +0545

    additions

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function void validate(){
+	var name= document.getElementByClass("name").value;
+	var description= document.getElementByClass("description").value;
+	var nameToMatch= /[a-z A-Z]/;
+	var descriptionToMatch= /[a-z A-Z 0-9 \.]/
+	
+	if(nameToMatch.test(name)){
+		return true;
+	}
+	else{
+		event.preventDefault();
+		windows.alert("The name should not include any special symbols or numbers.");
+		return false;
+	}
+
+	if(descriptionToMatch.test(description)){
+		return true;
+	}
+	else{
+		event.preventDefault();
+		return false; 
+	}
+
+
+}
